36e29cfe3b2c0879c131cb3c7a752eecd6a672ad
wnagxingyu <136959503+wnagxingyu@users.noreply.github.com>
2023-07-03 21:41:45 +0800

    Delete wxy directory

--- wxy/src/main/java/com/example/wxy/service/ThingsService.java
+++
@@ -1,7 +0,0 @@
-package com.example.wxy.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.wxy.entity.Things;
-
-public interface ThingsService extends IService<Things> {
-}
